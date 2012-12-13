7934f9d2c691d5df061e3459d804603083db4f23
josieliang <jliang@commonsense.org>
2012-12-13 01:19:17 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/test_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "CommonSenseMedia"
+maintainer_email "jliang@commonsense.org"
+license          "All rights reserved"
+description      "Installs/Configures test_cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "test_cookbook::default","Prints hello world output"

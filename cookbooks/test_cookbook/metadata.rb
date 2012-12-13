a4caab3396b27065ad410d049552062f14416ca8
root <root@ip-10-252-15-136.us-west-2.compute.internal>
2012-12-13 23:01:04 +0000

    Added rightscale_marker to default.rb

--- cookbooks/test_cookbook/metadata.rb
+++ cookbooks/test_cookbook/metadata.rb
@@ -5,4 +5,5 @@ description      "Installs/Configures test_cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rightscale"
 recipe "test_cookbook::default","Prints hello world output"
